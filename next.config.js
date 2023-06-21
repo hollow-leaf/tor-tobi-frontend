e56ec94d55ef4e7c95a599c1d9ec057162bf4fda
kidneyweak <kidneyweak@gmail.com>
2023-06-21 17:52:29 +0800

    chore: github page deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV !== 'development'
+
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
@@ -6,6 +8,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/frontend' : '',
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
