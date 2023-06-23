2172b758b87ef9c05c4a05c0123f7006ae1f1e9b
Brian <pianochicken1996@gmail.com>
2023-06-23 16:40:57 +0800

    fix: modify config and fix deposit.service for building

--- app/services/deposit.service.ts
+++ app/services/deposit.service.ts
@@ -1,26 +1,26 @@
-import { useContractWrite as useContractWriteWagmi, usePrepareContractWrite } from 'wagmi'
+import { useContractWrite as useContractWriteWagmi } from 'wagmi'
 import { DepositParameter } from '../appModel'
 import { useKamuiContractAddressHook } from '../../hooks/useContractAddress.hook'
-import { useContractWrite, useContract } from '@starknet-react/core'
+import { useContract } from '@starknet-react/core'
 import { kamuiABI } from '../contracts/Kamui'
 
 // Wagmi Contract
-const { data, isLoading, isSuccess, write } = useContractWriteWagmi({
-    address: useKamuiContractAddressHook(),
-    abi: kamuiABI,
-    functionName: 'deposit',
-  })
+// const { data, isLoading, isSuccess, write } = useContractWriteWagmi({
+//     address: useKamuiContractAddressHook(),
+//     abi: kamuiABI,
+//     functionName: 'deposit',
+//   })
 
-// StarkNet Contract
-const { contract } = useContract({
-    // address: ethAddress,
-    // abi: compiledErc20.abi
-})
+// // StarkNet Contract
+// const { contract } = useContract({
+//     // address: ethAddress,
+//     // abi: compiledErc20.abi
+// })
 
-export async function depositWagmi (payload: DepositParameter) {
-    console.log('Deposit Wagmi')
-} 
+// export async function depositWagmi (payload: DepositParameter) {
+//     console.log('Deposit Wagmi')
+// } 
 
-export async function depositStarkNet (payload: DepositParameter) {
-    console.log('Deposit StarkNet')
-} 
+// export async function depositStarkNet (payload: DepositParameter) {
+//     console.log('Deposit StarkNet')
+// } 
